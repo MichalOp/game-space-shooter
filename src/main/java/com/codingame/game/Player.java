413a5e99b163025821b50a20b49070927edcf70c
Katarzyna Miernikiewicz <kasiamanwi23@gmail.com>
2021-06-11 19:27:30 +0200

    changed S to P, full action names added

--- src/main/java/com/codingame/game/Player.java
+++ src/main/java/com/codingame/game/Player.java
@@ -84,33 +84,39 @@ public class Player extends AbstractMultiplayerPlayer {
                 try {
                     switch (scanner.next()) {
                         case "A":
+                        case "Accelerate":
                             // it's a move (for a ship or a player-controlled missile)
                             action.type = Action.ActionType.Move;
                             action.direction.x = getActionDirectionValue(scanner);
                             action.direction.y = getActionDirectionValue(scanner);
                             break;
                         case "F":
+                        case "Fire":
                             // fire a weapon in certain direction
                             action.type = Action.ActionType.Fire;
                             action.direction.x = getActionDirectionValue(scanner);
                             action.direction.y = getActionDirectionValue(scanner);
                             break;
                         case "M":
+                        case "Missile":
                             // fire a missile
                             action.type = Action.ActionType.Missile;
                             action.direction.x = getActionDirectionValue(scanner);
                             action.direction.y = getActionDirectionValue(scanner);
                             break;
                         case "D":
+                        case "Detonate":
                             // detonate missile
                             action.type = Action.ActionType.Detonate;
                             break;
                         case "W":
+                        case "Wait":
                             // wait - this is a dummy action, but we want to have it
                             // as we can only set a constant as a number of lines we expect to get from the player
                             action.type = Action.ActionType.Wait;
                             break;
-                        case "S":
+                        case "P":
+                        case "Print":
                             // say - get a message to print
                             message_text = scanner.nextLine();
                             break;
